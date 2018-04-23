ad9db168a07ab8d09868a57d657df48715cb9ad9
Simon Garnotel <simon.garnotel@gmail.com>
2018-04-23 11:52:21 +0200

    solve opencascade download

--- gmsh-docker-compilation/build.sh
+++ gmsh-docker-compilation/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+## Get opencascade
+wget https://www.dropbox.com/s/zbqlp2i2kmwwkiy/opencascade-7.2.0.tgz?dl=0
+
 ## Build the docker for Gmsh compilation
 echo -e "> Building the compiled version of Gmsh"
 sudo docker build --network=host --tag gmsh:compilation .
